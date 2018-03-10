a8c20d595c05270d5911f4bbe05b198504166de3
Nakamura Kosuke <ncl0709@gmail.com>
2018-03-10 22:19:28 +0900

    Notify when exceed the error count within specific_period_minutes

--- app/models/error_report.rb
+++ app/models/error_report.rb
@@ -94,8 +94,15 @@ class ErrorReport
   end
 
   def should_notify?
-    app.notification_service.notify_at_notices.include?(0) ||
-      app.notification_service.notify_at_notices.include?(@problem.notices_count)
+    if app.notification_service.use_notify_of_exceed
+      Notice.where(
+        err_id: @notice.err_id,
+        created_at: app.notification_service.specific_period_minutes..Time.now
+      ).count == app.notification_service.specific_error_count
+    else
+      app.notification_service.notify_at_notices.include?(0) ||
+        app.notification_service.notify_at_notices.include?(@problem.notices_count)
+    end
   end
 
   # Launch all notification define on the app associate to this notice
